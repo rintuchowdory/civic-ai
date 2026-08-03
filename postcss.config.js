8a53bd242923b6ee1a24173df4cda65a3ce05afe
Chowdory Rintu <chowdoryrintu60@gmail.com>
2026-08-03 23:11:19 +0200

    fix: remove unsupported Firefox CSS declaration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const removeUnsupportedWebkitTextSizeAdjust = {
+  postcssPlugin: "remove-unsupported-webkit-text-size-adjust",
+  Declaration(decl) {
+    if (decl.prop === "-webkit-text-size-adjust") {
+      decl.remove();
+    }
+  },
+};
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "remove-unsupported-webkit-text-size-adjust": removeUnsupportedWebkitTextSizeAdjust,
   },
 };
